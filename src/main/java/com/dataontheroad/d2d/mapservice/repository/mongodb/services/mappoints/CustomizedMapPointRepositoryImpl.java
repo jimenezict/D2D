bb01aec45363dda4866a2665038c19cad38932c5
jimenezict <jimenezict@gmail.com>
2019-05-30 19:22:03 +0200

    Refactor CustomizedMapPointRepositoryImpl

--- src/main/java/com/dataontheroad/d2d/mapservice/repository/mongodb/services/mappoints/CustomizedMapPointRepositoryImpl.java
+++ src/main/java/com/dataontheroad/d2d/mapservice/repository/mongodb/services/mappoints/CustomizedMapPointRepositoryImpl.java
@@ -1,14 +1,13 @@
 package com.dataontheroad.d2d.mapservice.repository.mongodb.services.mappoints;
 
 import com.dataontheroad.d2d.mapservice.repository.mongodb.services.mappoints.model.MapPointMongo;
-import com.dataontheroad.d2d.mapservice.restcontroller.message.PostRequest.Position;
 import com.dataontheroad.d2d.mapservice.restcontroller.message.PostRequest.RadialMessage;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.geo.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.NearQuery;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,22 +17,35 @@ public class CustomizedMapPointRepositoryImpl implements CustomizedMapPointRepos
     MongoTemplate mongoTemplate;
 
     public List<MapPointMongo> filterByCircle(RadialMessage radialMessage) {
-        Point point = new Point(radialMessage.getPosition().getX_cord(), radialMessage.getPosition().getY_cord());
-        Distance distance = new Distance(radialMessage.getMeters() / 1000, Metrics.KILOMETERS);
+        return convertMapPointsToList(getMapPointsByDistance(radialMessage));
+    }
 
-        GeoResults<MapPointMongo> geoMapPointMongo =
-                mongoTemplate
-                        .geoNear(NearQuery.near(point).maxDistance(distance).num(radialMessage.getNumResults() + 1),
-                        MapPointMongo.class, "MapPoints");
-
-        List<MapPointMongo> closerMapPointLists = geoMapPointMongo
+    @NotNull
+    private List<MapPointMongo> convertMapPointsToList(GeoResults<MapPointMongo> geoMapPointMongo) {
+        return geoMapPointMongo
                 .getContent()
                 .stream()
                 .map(x -> x.getContent())
                 .collect(Collectors.toList());
+    }
 
-        return closerMapPointLists;
+    private GeoResults<MapPointMongo> getMapPointsByDistance(RadialMessage radialMessage) {
+        Point point = getPoint(radialMessage);
+        Distance distance = getDistance(radialMessage);
 
+        return mongoTemplate
+                .geoNear(NearQuery.near(point).maxDistance(distance).num(radialMessage.getNumResults() + 1),
+                MapPointMongo.class, "MapPoints");
+    }
+
+    @NotNull
+    private Distance getDistance(RadialMessage radialMessage) {
+        return new Distance(radialMessage.getMeters() / 1000, Metrics.KILOMETERS);
+    }
+
+    @NotNull
+    private Point getPoint(RadialMessage radialMessage) {
+        return new Point(radialMessage.getPosition().getX_cord(), radialMessage.getPosition().getY_cord());
     }
 
 }
