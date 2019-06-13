d5547bd6526380b6d235bf914c4c62348331d8a5
jimenezict <jimenezict@gmail.com>
2019-06-13 22:54:27 +0200

    Adding Logger to Statistics Service

--- src/main/java/com/dataontheroad/minigis/statistics/StatisticsController.java
+++ src/main/java/com/dataontheroad/minigis/statistics/StatisticsController.java
@@ -2,6 +2,8 @@ package com.dataontheroad.minigis.statistics;
 
 import com.dataontheroad.minigis.statistics.message.StatisticsResponse;
 import com.dataontheroad.minigis.statistics.service.Statistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +15,12 @@ public class StatisticsController {
     @Autowired
     private Statistics statistics;
 
+    Logger logger = LoggerFactory.getLogger(StatisticsController.class);
+
     @GetMapping("/statistics")
     @ResponseBody
     public StatisticsResponse getStatistics(){
+        logger.info("Call to Get Statistics Controller REST");
         return new StatisticsResponse(statistics.getStatistics());
     }
 }
--- src/main/java/com/dataontheroad/minigis/statistics/service/StatisticsImpl.java
+++ src/main/java/com/dataontheroad/minigis/statistics/service/StatisticsImpl.java
@@ -2,14 +2,19 @@ package com.dataontheroad.minigis.statistics.service;
 
 import com.dataontheroad.minigis.statistics.repository.StatisticsDataService;
 import com.dataontheroad.minigis.statistics.service.model.StatisticsDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class StatisticsImpl implements Statistics {
 
     @Autowired
     private StatisticsDataService statisticsDataService;
+
+    Logger logger = LoggerFactory.getLogger(StatisticsImpl.class);
 
     public void setStatisticsDataService(StatisticsDataService statisticsDataService) {
         this.statisticsDataService = statisticsDataService;
@@ -18,8 +23,12 @@ public class StatisticsImpl implements Statistics {
     @Override
     public StatisticsDTO getStatistics() {
         StatisticsDTO statistic = statisticsDataService.getStatistics();
-        if(statistic == null)
-            return new StatisticsDTO(0,0,0);
+        if(statistic == null) {
+            logger.info("Get Statistics Service: Not Statistics on Database");
+            return new StatisticsDTO(0, 0, 0);
+        }
+        logger.debug("Get Statistics Service: countries {}, items {}, users {} ",
+                statistic.getCountries(),statistic.getItems(),statistic.getUsers());
         return statistic;
     }
 }
